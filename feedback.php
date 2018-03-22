345b58689d9edf367f016c3d5629b82e04d6f2ee
GVSU-yenj <yenj@mail.gvsu.edu>
2018-03-22 15:17:35 -0400

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,6 +1,6 @@
 
 <?php
-$mailto = "student@mail.gvsu.edu";//Enter YOUR email address here surrounded by the quotes. Before testing the form on the EOS machines, be sure the permissions for this file are set (page 18 of the Setup Website Hosting instructions). Also, if you do not immediately receive an email, check your junk folder. Do not change anything else in this php file except for your email address. Doing so can cause problems on the server and Lawrence, our server administrator, will not be a happy camper.
+$mailto = "yenj@mail.gvsu.edu";//Enter YOUR email address here surrounded by the quotes. Before testing the form on the EOS machines, be sure the permissions for this file are set (page 18 of the Setup Website Hosting instructions). Also, if you do not immediately receive an email, check your junk folder. Do not change anything else in this php file except for your email address. Doing so can cause problems on the server and Lawrence, our server administrator, will not be a happy camper.
 $subject = "Contact Form"; 
 $header = "From: ".$_POST['email'];
 foreach ($_POST as $key => $value)
